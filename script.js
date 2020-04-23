0cd19b4236d9694f03eff91feb0fa47e4d9a2793
Elvin Nunez <eanunez25@gmail.com>
2020-04-23 17:55:44 -0500

    more quotes and greetings

--- script.js
+++ script.js
@@ -1,44 +1,20 @@
-let quotesArr = [
-    `“Productivity is being able to do things that you were never able to do before.” ~ Franz Kafka`,
-    `“Life is too complicated not to be orderly.” ~ Martha Stewart`, 
-    `“While one person hesitates because he feels inferior, the other is busy making mistakes and becoming superior.” —Henry Link`,
-    `“The winners in life think constantly in terms of I can, I will, and I am. Losers, on the other hand, concentrate their waking thoughts on what they should have or would have done, or what they can’t do.” ~ Dennis Waitley`,
-    `“You can’t be that kid standing at the top of the waterslide, overthinking it. You have to go down the chute.” —Tina Fey`,
-    `“Amateurs sit and wait for inspiration, the rest of us just get up and go to work.” ~ Stephen King`,
-    `“It always seems impossible until it’s done.” —Nelson Mandela`,
-    `Push yourself, because no one else is going to do it for you.`,
-    `Sometimes later becomes never. Do it now.`,
-    `“Your mind is for having ideas, not holding them.” —David Allen`,
-    `“If you spend too much time thinking about a thing, you’ll never get it done.” ~ Bruce Lee`,
-    `“Absorb what is useful, reject what is useless, add what is specifically your own.” ~ Bruce Lee`,
-    `“I feel that luck is preparation meeting opportunity.” ~ Oprah Winfrey`,
-    `Great things never come from comfort zones.`,
-    `“By failing to prepare, you are preparing to fail.” ~ Benjamin Franklin`,
-    `“It is not the strongest of the species that survive, nor the most intelligent, but the one most responsive to change.” ~ Charles Darwin`,
-    `“My goal is no longer to get more done, but rather to have less to do.” ~ Francine Jay`,
-    `“You see, in life, lots of people know what to do, but few people actually do what they know. Knowing is not enough! You must take action.” —Tony Robbins`,
-    `“Live daringly, boldly, fearlessly. Taste the relish to be found in competition – in having put forth the best within you.” ~ Henry J. Kaiser`,
-    `Start by doing what’s necessary; then do what’s possible; and suddenly you are doing the impossible. – Francis Of Assisi`,
-    `The way to get started is to quit talking and begin doing. – Walt Disney`,
-    `It’s not that I’m so smart, it’s just that I stay with problems longer. – Albert Einstein`,
-    `Sometimes, things may not go your way, but the effort should be there every single night. – Michael Jordan`,
-    `“If passion drives you, let reason hold the reins.”– Benjamin Franklin`,
-    `“The greater the obstacle, the more glory in overcoming it.”– Jean-Baptiste Poqeulin (Moliere)`,
-    `“You miss 100% of the shots you don’t take.”– Wayne Gretzky`,
-    `“Procrastination is opportunity’s assassin.” —Victor Kiam`,
-    `"I am Beyonce always"`,
-    `"When you come to a fork in the road, take it" -Yogi Berra`,
-    `"Third time's a charm."`
+let toBeSorted = [
+    
 ];
 
 let cheQuotes = [
+    `"Its been 'one of those days' for three years now." -daily Elvin thought`,
+    `"A relationship without trust is like a phone without service. And what do you do with a phone that doesn't have service? Play games." -Drake, street poet and philosopher`,
+    `"If you don't know where you are going, you might end up someplace else." -Yogi Berra`,
     `"Hello world, I'm with a yellow girl, number two pencil"  -Lil Wayne, James also`,
+    `“Of all the trees we could have hit, we had to get the one that hits back.” -Ronald Weasley`,
     `"Third time's a charm."`,
     `"No." -Rosa Parks`,
+    `“If a man is called to be a street sweeper, he should sweep streets even as a Michaelangelo painted, or Beethoven composed music or Shakespeare wrote poetry. He should sweep streets so well that all the hosts of heaven and earth will pause to say, 'Here lived a great street sweeper who did his job well.” -Dr. Martin Luther King Jr.`,
     `"I got a Glock in my 'Rari" -Fetty Wap`,
-    `"If you don't know where you are going, you might end up someplace else." -Yogi Berra`,
-    `"Baseball is 90% mental and the other half is physical" -Yogi Berra, baseball and almost everything else in life`,
-    
+    `"Baseball is 90% mental and the other half is physical" -Yogi Berra, on baseball and almost everything else in life`,
+    `"There was no collusion. Everybody knows there was no collution." -Donald Trump after his former lawyer, Michael Cohen, was found guilty of 8 forms of fraud, a former campaign chairman, Paul Manafort, who was too found guilty of 8 forms of fraud, another former campaign chairman, George Papadapalous, was found guilty lying to FBI agents, a Dutch attorney, Alex van der Zwaan, who lied to the FBI for Manafort, former Trump advisor, Roger Stone, was caught tampering and lying to Congress, former Trump advisor, Rick Gates, guilty to conspiracy and lying to the FBI, and still awaiting sentencing, Michael Flynn, the former national security advisor that sold out our country`,
+    `"18 years man, 18 years. The baby mama's car and crib is bigger than his. You turn on the TV on any given Sunday, win the Superbowl and drive off in a Hyudai. She was supposed to buy your shorty Tyco with your money. Then she went to the doctor and got lipo with your money." -Lebron James on using protection`,
 ]
 
 let anytimeGreetings = [
@@ -47,14 +23,39 @@ let anytimeGreetings = [
     "Hello",
     "Salutations",
     "Sup",
-    "Howdy",
-    "Bonjour",
     "Hola", 
     "Konnichiwa",
     "Kamusta",
     "WAZZAAAAAAP",
-    "Que Onda"
+    "Blessings", 
+    "Stay Classy",
+    "Wash Your Hands",
 ];
+
+let morningGreetings = [
+    "Good Morning",
+    "Wake Up",
+    "Wakey Wakey",
+    "Mornin'", 
+    "You Survived The Night",
+]
+
+let afternoonGreetings = [
+    "Good Afternoon",
+    "It's Lunch Time", 
+    "Grind Time", 
+    "Walk Nutella",
+]
+
+let nightGreetings = [
+    "Good Evening",
+    "Good Night",
+    "Go To Sleep",
+    "Au Revoir",
+    "G'night",
+    "Night",
+    "Turn Up", 
+]
 
 let nicknames = [
     "Che",
@@ -69,9 +70,36 @@ let nicknames = [
     "Rachael"
 ];
 
+let timebasedArray = [];
+timebasedGreeting = () =>{
+    let time = new Date().getHours();
+    if (time >= 5 && time <= 11) {
+        timebasedArray = morningGreetings.concat(anytimeGreetings);
+    } else if (time >= 12 && time <= 18) {
+        timebasedArray = anytimeGreetings.concat(afternoonGreetings);
+    } else {
+        timebasedArray = anytimeGreetings.concat(nightGreetings);
+    }
+    return timebasedArray;
+}
+console.table(timebasedGreeting());
+
 const greeting = document.querySelector(".statement");
 randomGreeting = () => {
-    greeting.textContent = anytimeGreetings[Math.floor(Math.random() * anytimeGreetings.length)];
+    greeting.textContent = timebasedGreeting()[Math.floor(Math.random() * timebasedGreeting().length)];
     greeting.textContent += ", " + nicknames[Math.floor(Math.random() * nicknames.length)];
 }
 randomGreeting();
+
+console.table(cheQuotes);
+
+let launchDate = new Date();
+launchDate.setFullYear(2020, 3, 23);
+let today = new Date();
+let dayDifference = today.getDate() - launchDate.getDate();
+
+const quoteBox = document.querySelector(".quote");
+todaysQuote = () => {
+    quoteBox.textContent = cheQuotes[dayDifference];
+}
+todaysQuote();

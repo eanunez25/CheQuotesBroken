0c8a59267ac6e275a51c58519f3c594ec4895218
Elvin Nunez <eanunez25@gmail.com>
2020-08-07 15:53:50 -0500

    add quote and change image size for mobile

--- js.js
+++ js.js
@@ -609,7 +609,7 @@ dayDifference = (today - launchDate) / day;
 
 const quoteBox = document.querySelector(".quote");
 todaysQuote = () => {
-    quoteBox.innerHTML = cheQuotes[dayDifference+1];
+    quoteBox.innerHTML = cheQuotes[dayDifference];
 }
 todaysQuote();
 
